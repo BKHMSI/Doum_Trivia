c7cd2ace658da8fb2083ac3d88753d4587db5502
BKHMSI <badr@khamissi.com>
2017-07-01 04:18:44 +0200

    get started button

--- index.js
+++ index.js
@@ -38,36 +38,33 @@ app.listen(app.get('port'), function() {
 });
 
 // Set FB bot greeting text
-// facebookThreadAPI('./fb-greeting-text.json', 'Greating Text');
+facebookThreadAPI('./fb-greeting-text.json', 'Greating Text');
 // Set FB bot get started button
-// facebookThreadAPI('./fb-get-started-button.json', 'Get Started Button');
+facebookThreadAPI('./fb-get-started-button.json', 'Get Started Button');
 // Set FB bot persistent menu
-// facebookThreadAPI('./fb-persistent-menu.json', 'Persistent Menu');
-
-// // mongodb://bkhmsi:sa7walaghalat@ds141242.mlab.com:41242/doum_trivia
-const token = process.env.FB_PAGE_TOKEN;
+facebookThreadAPI('./fb-persistent-menu.json', 'Persistent Menu');
 
 // Calls the Facebook graph api to change various bot settings
-// function facebookThreadAPI(jsonFile, cmd){
-//     // Start the request
-//     request({
-//         url: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+process.env.FB_PAGE_TOKEN,
-//         method: 'POST',
-//         headers: {'Content-Type': 'application/json'},
-//         form: require(jsonFile)
-//     },
-//     function (error, response, body) {
-//         if (!error && response.statusCode == 200) {
-//             // Print out the response body
-//             console.log(cmd+": Updated.");
-//             console.log(body);
-//         } else { 
-//             // TODO: Handle errors
-//             console.log(cmd+": Failed. Need to handle errors.");
-//             console.log(body);
-//         }
-//     });
-// }
+function facebookThreadAPI(jsonFile, cmd){
+    // Start the request
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/thread_settings?access_token='+process.env.FB_PAGE_TOKEN,
+        method: 'POST',
+        headers: {'Content-Type': 'application/json'},
+        form: require(jsonFile)
+    },
+    function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log(cmd+": Updated.");
+            console.log(body);
+        } else { 
+            // TODO: Handle errors
+            console.log(cmd+": Failed. Need to handle errors.");
+            console.log(body);
+        }
+    });
+}
 
 function sendAPI(sender, msg){
     request({
