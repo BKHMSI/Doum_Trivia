3d2638ccab5ea8bdd9d5e520bf7fc78f3b5b302d
BKHMSI <badr@khamissi.com>
2017-07-03 19:05:56 +0200

    fixing bug

--- index.js
+++ index.js
@@ -232,7 +232,7 @@ function checkAnswerAndUpdate(sender, answer){
 			var options = {upsert: true};
 			var update = {
 				user_id: sender,
-				category: category,
+				category: obj.category,
 				q_id: obj.q_id,
 				score: isCorrect ? obj.score+1:obj.score,
 				count: obj.count + 1,
